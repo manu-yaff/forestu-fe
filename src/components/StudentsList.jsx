cd45c4f63b41052caeb95bea4b1f5784f56a5f3a
manu-yaff <manuelyg2097@gmail.com>
2023-11-19 21:24:47 -0600

    chore: add search for students

--- src/components/StudentsList.jsx
+++ src/components/StudentsList.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 
 export const StudentsList = () => {
   const [students, setStudents] = useState([]);
+  const [filteredStudents, setFilteredStudents] = useState([]);
   const { id } = useParams();
 
   async function fetchTeacherGroups() {
@@ -13,20 +14,33 @@ export const StudentsList = () => {
     return jsonResponse;
   }
 
+  function filterStudents(event) {
+    console.log(event.target.value);
+    const filteredStudents = students.filter((student) =>
+      student.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    console.log(filteredStudents);
+    setFilteredStudents(filteredStudents);
+  }
+
   useEffect(() => {
     (async () => {
       const { data } = await fetchTeacherGroups();
       setStudents(data);
+      setFilteredStudents(data);
     })();
   }, []);
 
   return (
-    <ul>
-      {students.map((student) => (
-        <Link to={`/teacher/students/${student.id}`} key={student.id}>
-          {student.name}
-        </Link>
-      ))}
-    </ul>
+    <>
+      <input onChange={(event) => filterStudents(event)} />
+      <ul>
+        {filteredStudents.map((student) => (
+          <Link to={`/teacher/students/${student.id}`} key={student.id}>
+            {student.name}
+          </Link>
+        ))}
+      </ul>
+    </>
   );
 };
